9e1d5d4cffe991ef8fce0f293363b29228560dd0
gajjarpratik <gajjar@bu.edu>
2013-04-02 23:43:20 -0400

    test

---
+++ Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Email: <asp:TextBox ID="Email" runat="server"></asp:TextBox><br />
+        Password: <asp:TextBox ID="Password" runat="server"></asp:TextBox><br />
+        <asp:Button ID="Login" runat="server" Text="Login" OnClick="Login_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Login_Click(object sender, EventArgs e)
+    {
+        string email = Email.Text;
+        string password = Password.Text;
+
+        //Connection String
+        string connString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Pratik\digicourse\App_Data\Database.mdf;Integrated Security=True";
+        string query = "SELECT password FROM login WHERE email = @email";
+        string p = null;
+
+        //Get Connection
+        SqlConnection conn = new SqlConnection(connString);
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+        cmd.CommandText = query;
+        cmd.Parameters.AddWithValue("email", email);
+
+        conn.Open();
+
+        //Execute query
+        SqlDataReader rdr = cmd.ExecuteReader();
+
+        while (rdr.Read())
+        {
+            p = rdr["password"].ToString();
+        }
+        conn.Close();
+
+        if (password.Equals(p))
+        {
+            Server.Transfer("Welcome.aspx");
+        }
+        else
+        {
+            Server.Transfer("Default.aspx");
+        }
+        
+    }
+}
